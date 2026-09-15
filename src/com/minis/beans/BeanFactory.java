6a873d6ed2087571c385ddfea0dd5fb54de4e1d1
qingshan.shi <sqsjg@163.com>
2026-09-15 08:37:13 +0800

    formate code

--- src/com/minis/beans/BeanFactory.java
+++ src/com/minis/beans/BeanFactory.java
@@ -2,7 +2,7 @@ package com.minis.beans;
 
 public interface BeanFactory {
 
-	Object getBean(String beanName) throws NoSuchBeanDefinitionException;
+    Object getBean(String beanName) throws NoSuchBeanDefinitionException;
 
-	void registerBeanDefinition(BeanDefinition bd);
+    void registerBeanDefinition(BeanDefinition bd);
 }
--- src/com/minis/beans/SimpleBeanFactory.java
+++ src/com/minis/beans/SimpleBeanFactory.java
@@ -5,40 +5,40 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SimpleBeanFactory implements BeanFactory{
-    private List<BeanDefinition> beanDefinitions=new ArrayList<>();
-    private List<String> beanNames=new ArrayList<>();
-    private Map<String, Object> singletons =new HashMap<>();
+public class SimpleBeanFactory implements BeanFactory {
+    private List<BeanDefinition> beanDefinitions = new ArrayList<>();
+    private List<String> beanNames = new ArrayList<>();
+    private Map<String, Object> singletons = new HashMap<>();
 
     public SimpleBeanFactory() {
     }
 
-    public Object getBean(String beanName) throws NoSuchBeanDefinitionException{
+    public Object getBean(String beanName) throws NoSuchBeanDefinitionException {
         Object singleton = singletons.get(beanName);
         if (singleton == null) {
-        	int i = beanNames.indexOf(beanName);
-        	if (i == -1) {
-        		throw new NoSuchBeanDefinitionException();
-        	}
-        	else {
-        		BeanDefinition bd = beanDefinitions.get(i);
-        		try {
-            		singleton=Class.forName(bd.getClassName()).newInstance();
-				} catch (InstantiationException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				}
-				singletons.put(bd.getId(),singleton);
-        	}       	
+            int i = beanNames.indexOf(beanName);
+            if (i == -1) {
+                throw new NoSuchBeanDefinitionException();
+            } else {
+                BeanDefinition bd = beanDefinitions.get(i);
+                try {
+                    singleton = Class.forName(bd.getClassName()).newInstance();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                singletons.put(bd.getId(), singleton);
+            }
         }
         return singleton;
     }
-    public void registerBeanDefinition(BeanDefinition bd){
-    	this.beanDefinitions.add(bd);
-    	this.beanNames.add(bd.getId());
+
+    public void registerBeanDefinition(BeanDefinition bd) {
+        this.beanDefinitions.add(bd);
+        this.beanNames.add(bd.getId());
     }
-    
+
 }
